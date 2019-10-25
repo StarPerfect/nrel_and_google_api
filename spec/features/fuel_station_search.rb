73c287e7d2a239a453a02a7c4537d5140eb5506d
Corina Allen <StarPerfect@gmail.com>
2019-10-25 09:44:15 -0600

    Update spec with details expected on results page

--- spec/features/fuel_station_search.rb
+++ spec/features/fuel_station_search.rb
@@ -34,5 +34,14 @@ describe 'Fuel Station Search Endpoint' do
     click_button 'Find Nearest Station'
 
     expect(current_path).to eq(search_path)
+    within('.NREL') do
+      expect(page).to have_content('Seventeenth Street Plaza')
+      expect(page).to have_content('1225 17th St.')
+      expect(page).to have_content('Denver')
+      expect(page).to have_content('CO')
+      expect(page).to have_content('80202')
+      expect(page).to have_content('ELEC')
+      expect(page).to have_content('Not Specified')
+    end
   end
 end
