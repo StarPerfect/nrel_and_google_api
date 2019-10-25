67b5d0931f3ea2babf1d3a72153c9d5d6770a2d6
Corina Allen <StarPerfect@gmail.com>
2019-10-25 11:28:12 -0600

    Update SearchController with Google API data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,9 +13,21 @@ class SearchController < ApplicationController
 
     parsed = JSON.parse(raw_response.body, sybmolize_names: true)
 
-    fuel_station = FuelStation.new(parsed)
-
     @result = FuelStation.new(parsed)
+
+    google_conn = Faraday.new(
+      url: 'https://maps.googleapis.com/maps/api/',
+      params: { key: ENV['GOOGLE_API_KEY'],
+                origin: location_params,
+                destination: "#{@result.street}, #{@result.city}, #{@result.state} #{@result.zip}"
+      })
+
+    google_response = google_conn.get('directions/json')
+
+    google_parsed = JSON.parse(google_response.body, sybmolize_names: true)
+
+    @route = GoogleRoute.new(google_parsed)
+
   end
 
   private
