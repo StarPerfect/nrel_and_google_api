773a821064d68ba6dd8717a6a59fc180150faa11
Corina Allen <StarPerfect@gmail.com>
2019-10-25 09:44:38 -0600

    Update SearchController with Postman working Endpoint for reference

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
-  end 
+  end
 end
+
+# 
+# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=ELEC&location=1331 17th St LL100, Denver, CO 80202&limit=1&api_key=jZktIqsE2gojvKnQbNJE6amj88A36FbqoliZ5agQ
