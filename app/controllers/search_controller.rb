30ce4eface59bec72aee088aecc7209cb34ba75c
Corina Allen <StarPerfect@gmail.com>
2019-10-25 10:05:35 -0600

    update SearchController#index with Faraday connection and JSON parsing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,17 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(
+    url: 'https://developer.nrel.gov/',
+    params: { fuel_type: 'ELEC',
+              location: '1331 17th St LL100, Denver, CO 80202',
+              limit: 1,
+              api_key: ENV['NREL_API_KEY'],
+              format: 'JSON'}
+    )
+
+    raw_response = conn.get('/api/alt-fuel-stations/v1/nearest.json')
+
+    parsed = JSON.parse(raw_response.body, sybmolize_names: true)
+    binding.pry
   end
 end
-
-# 
-# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=ELEC&location=1331 17th St LL100, Denver, CO 80202&limit=1&api_key=jZktIqsE2gojvKnQbNJE6amj88A36FbqoliZ5agQ
