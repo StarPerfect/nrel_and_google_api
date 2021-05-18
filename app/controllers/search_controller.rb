9a52775f3f19fec79c9f2887c09d30a84347f669
Ben <benjamin.fulton99@gmail.com>
2021-05-18 17:41:17 -0600

    Unrefactored code state

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,12 @@
 class SearchController < ApplicationController
-  
+  def index
+    # Input validation / type checking 
+
+    origin_point = params[:location]
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{origin_point}&fuel_type=ELEC&limit=1")
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    directions_response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{origin_point}&destination=#{json[:latitude]},#{json[:longitude]}&key=#{ENV['GOOGLE_API_KEY']}")
+    binding.pry
+  end
 end
