eb0b1a7aee4d06a05047926418eff57a48686322
Ben <benjamin.fulton99@gmail.com>
2021-05-26 14:21:22 -0600

    Creates Structs to serve

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  Station = Struct.new(:name,:street, :city,:state,:zip,:fuel_type,:access_times)
+  Distance = Struct.new(:distance, :duration, :directions)
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,18 @@ class SearchController < ApplicationController
     # Input validation / type checking 
     
     origin_point = location_params[:location]
-    station_result = NrelService.get_stations(origin_point)
-    direction_result = GoogleService.get_directions.get({origin_point: origin_point, 
+    station_result = NrelService.get_station(origin_point)
+    direction_result = GoogleService.get_directions({origin_point: origin_point, 
                                                             lat: station_result[:latitude], 
                                                             lng: station_result[:longitude]})
-    binding.pry
+    @result = Station.new(station_result[:station_name], 
+                          station_result[:street_address], 
+                          station_result[:city],
+                          station_result[:state],
+                          station_result[:zip],
+                          station_result[:fuel_type_code],
+                          station_result[:access_days_time])
+    @route = Distance.new(direction_result[:distance], direction_result[:duration][:text], direction_result[:steps])                 
   end
 
   private
